c3d2e3e191bb0b69c50665ffbe292f8863bf3934
Jake Park <danbi2990@gmail.com>
2018-11-20 10:59:00 +0900

    Add newslab_count

---
+++ project/sandbox/newslab_count.py
@@ -0,0 +1,24 @@
+from random import randint
+from time import sleep
+import requests
+
+url = 'http://newslabit.hankyung.com/article/201811159793G'
+cookies = dict(
+    RMID='d260dae55b5fc520',
+    _ga='GA1.2.383747520.1533690677',
+    __gads='ID=93462fce992663ac:T=1535068086:S=ALNI_Mapry6fWAmMybtErkTYX85j7btD4Q',
+    cleanView='check',
+    hk_jamidusu='1%7C%EC%96%91%EB%A0%A5%7C198909190000',
+    _gid='GA1.2.2011354116.1541471770',
+    PHPSESSID='gkjou8sdt6mmhku8qml40g75op',
+    period='thisYear',
+    isRevisit='true',
+    intra='lNHw5qrs7f3ZbRdbBqLb8pm9Jmkp9m1wIUKd9CRrVcB9UJS7KdXaBTvT3DgSVgifMWz0c3VoZHyAu%2BR6xPaMuQ%3D%3D',
+    push='E%C2%0F%BB%07%04%29L%2B%C8%8C3%C6%A7%1EP%96%86%2C%C2%E2%A2%3A%DF%F0D%AD%2A%17%1A%AED%F4%3E',
+    gtmdlkr='',
+)
+
+for i in range(2000):
+    r = requests.get(url, cookies=cookies)
+    sleep(randint(4, 7))
+    print(r.status_code)
