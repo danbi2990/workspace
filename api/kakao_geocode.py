2e5a35bd71f7ac7239fcc6059b77f4af1b30268c
Jake Park <danbi2990@gmail.com>
2018-12-06 17:06:45 +0900

    Edit cvs: without_comma

--- api/kakao_geocode.py
+++ api/kakao_geocode.py
@@ -63,11 +63,18 @@ def get_cvs_geocode(road_addr, loc_name):
     if road_addr and not np.NaN:
         without_paren = re.search(r'(.*)\(.+\)', road_addr)
         if without_paren:
-            addr = without_paren.group(1)
+            # addr = without_paren.group(1)
             # print(addr)
-            res = fetch_geo_response_from_kakao(addr)
-            if res:
-                return res
+            road_addr = without_paren.group(1)
+
+        without_comma = re.search(r'(.*),.+', road_addr)
+        if without_comma:
+            road_addr = without_comma.group(1)
+
+        res = fetch_geo_response_from_kakao(road_addr)
+        if res:
+            return res
+
     if loc_name:
         res = fetch_geo_response_from_kakao(loc_name, url_type='keyword')
         if res:
