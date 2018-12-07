45dad385e2e9a4eeec88b0f789da91fc154dbc27
Jake Park <danbi2990@gmail.com>
2018-12-07 17:03:38 +0900

    Edit cvs kakao

--- project/cvs/a1_fill_geo_location.py
+++ project/cvs/a1_fill_geo_location.py
@@ -19,13 +19,16 @@ print(f'cvs_tobacco: {len(cvs_tobacco)}')  # before: 67602, after: 65541
 
 i = 0
 buffer_ = []
+error_ = []
 
 for idx, row in cvs_tobacco.loc[cvs_tobacco['lat'].isna()].iterrows():
+    sleep(0.1)
     road_addr = row['도로명전체주소']
     old_addr = row['소재지전체주소']
     name = row['사업장명']
     doc = None
     res = {}
+    err = {}
     doc = get_cvs_geocode(road_addr, old_addr, name)
     if doc:
         res['관리번호'] = row['관리번호']
@@ -36,6 +39,15 @@ for idx, row in cvs_tobacco.loc[cvs_tobacco['lat'].isna()].iterrows():
         print(road_addr)
         print(old_addr)
         print(name)
+        err['관리번호'] = row['관리번호']
+        err['사업장명'] = name
+        err['도로명전체주소'] = road_addr
+        err['소재지전체주소'] = old_addr
+        error_.append(err)
+
+        with MyMongo() as db:
+            db.update_one_bulk('cvs', 'cvs', error_, '관리번호')
+        error_.clear()
         # cvs_tobacco.at[idx, 'lat'] = doc['y']
         # cvs_tobacco.at[idx, 'lng'] = doc['x']
 
