9ce00b5a0f2972fc29dd5c11fcd5e705d50f7dab
Jake Park <danbi2990@gmail.com>
2018-12-26 10:11:38 +0900

    Add LG25, 엘지25, 위드미, 대상유통. Add budget project

---
+++ project/budget/a1.py
@@ -0,0 +1,60 @@
+from collections import defaultdict
+from collections import Counter
+
+from konlpy.tag import Mecab
+import pandas as pd
+
+directory = '/Users/jake/OneDrive - leverage innovative users/Documents/예산/'
+file = '2019년_경제정책방향.txt'
+
+with open(directory + file, 'r') as f:
+    raw = f.read()
+
+mecab = Mecab()
+list_of_pos = mecab.pos(raw)
+
+dict_of_pos = defaultdict(list)
+
+for p in list_of_pos:
+    dict_of_pos[p[1]].append(p[0])
+
+# print(len(dict_of_pos.keys()))
+# print(dict_of_pos['NNG'])
+# print(dict_of_pos['NNG'])
+
+counters = {}
+
+for tag, words in dict_of_pos.items():
+    cnter = Counter(words)
+    counters[tag] = cnter
+
+# print(len(counters.keys()))
+print(counters['NNG'])
+
+# df = pd.DataFrame.from_dict(counters)
+# print(df[['NNG','NNP']].head(5))
+
+# nng_counter = Counter(dict_of_pos['NNG'])
+# nnp_counter = Counter(dict_of_pos['NNP'])
+# vv_counter = Counter(dict_of_pos['VV'])
+# print(nng_counter.most_common())
+
+"""
+NNG 일반 명사
+NNP 고유 명사
+NNB 의존 명사
+NNBC    단위를 나타내는 명사
+NR  수사
+NP  대명사
+
+VV  동사
+
+VA  형용사
+VX  보조 용언
+VCP 긍정 지정사
+VCN 부정 지정사
+
+MM  관형사
+MAG 일반 부사
+MAJ 접속 부사
+"""
--- project/cvs/import_cvs_from_tobacco.py
+++ project/cvs/import_cvs_from_tobacco.py
@@ -1,37 +1,24 @@
-#%%
 import re
 import pandas as pd
-#%%
+
 directory_path = '/Users/jake/OneDrive - leverage innovative users/Documents/편의점/'
 file_name = 'tobacco_raw.xlsx'
 file_path = directory_path + file_name
 
-#%%
-# cvs.columns
 sheet1 = '담배소매업_1'
 tobacco1 = pd.read_excel(file_path, sheet1)
 len(tobacco1)
 
-#%%
-# tobacco1.head()
 cvs_names = [
-    '지에스', 'GS',
+    '지에스', 'GS', 'LG25', '엘지25',
     '씨유', 'CU', '훼미리마트', '훼밀리마트', '패밀리마트', 'familymart',
     '세븐일레븐', '코리아세븐', '바이더웨이', 'buytheway',
-    '이마트24', 'emart24',
-    '미니스톱', '미니스탑'
+    '이마트24', 'emart24', '위드미',
+    '미니스톱', '미니스탑', '대상유통',
     '씨스페이스', '씨스페이시스',
 ]
 cvs_string = '|'.join(cvs_names)
 
-#%%
-# big_cvs = pd.DataFrame()
-# for idx, row in tobacco1.iterrows():
-#     for name_keyword in cvs_names:
-#         if name_keyword in row['사업장명']:
-#             big_cvs = big_cvs.append(tobacco1.loc[idx])
-
-#%%
 sheet2 = '담배소매업_2'
 tobacco = tobacco1.append(pd.read_excel(file_path, sheet2))
 print(len(tobacco))
@@ -39,49 +26,6 @@ print(len(tobacco))
 idx_cvs = tobacco['사업장명'].str.contains(cvs_string, flags=re.IGNORECASE, regex=True)
 
 
-cvs_file_name = 'cvs_from_tobacco3.tsv'
-# big_cvs.to_csv(directory_path + cvs_file_name, sep='\t', index=False)
+cvs_file_name = 'cvs_from_tobacco4.tsv'
 print(len(tobacco.loc[idx_cvs]))
 tobacco.loc[idx_cvs].to_csv(directory_path + cvs_file_name, sep='\t', index=False)
-
-#%%
-
-
-
-# big_cvs = pd.DataFrame()
-# for idx, row in tobacco.iterrows():
-#     for name_keyword in cvs_names:
-#         if name_keyword in row['사업장명']:
-#             big_cvs = big_cvs.append(tobacco.loc[idx])
-
-#%%
-# big_cvs3 = big_cvs.append(big_cvs2)
-# len(big_cvs3)
-
-#%%
-# cvs_file_name = 'tobacco_from_cvs2.tsv'
-# big_cvs.to_csv(directory_path + cvs_file_name, sep='\t', index=False)
-
-#%%
-
-
-#%%
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
